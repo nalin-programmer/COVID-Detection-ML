b6e50e821421385fd3c394d6ec2c7cbfefed7709
Prerit Kumar Jha <preritkrjha@gmail.com>
2021-09-22 12:37:11 +0530

    UPLOAD IMAGE

---
+++ src/Activity.js
@@ -0,0 +1,126 @@
+import React from "react";
+import "./Activity.scss";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+toast.configure();
+
+export const Activity = () => {
+  const [image, setimage] = useState("");
+  const [imgUrl, setimgUrl] = useState("");
+  var url = "";
+
+  function PostData(e) {
+    e.preventDefault();
+    console.log("POSTING DATA...");
+    const fileType = image["type"];
+    const validImageTypes = ["image/jpg", "image/jpeg", "image/png"];
+    if (!validImageTypes.includes(fileType)) {
+      // alert('invalid image format');
+      toast.error("Invalid Image Type, Retry ?", {
+        position: toast.POSITION.TOP_CENTER,
+      });
+      return;
+    }
+
+    var fileSize = image["size"];
+    if (fileSize > 1000000) {
+      //  alert('Photo Size Exceeds , Size must be less than 500Kb');
+      toast.error("Photo Size Exceeds , Size must be less than 1MB", {
+        position: toast.POSITION.TOP_CENTER,
+      });
+      return;
+    }
+
+    toast.info("Image Uploading ! Plese Wait !", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "gifter");
+    data.append("cloud_name", "prerit-cloud");
+    async function API() {
+      const responce = await fetch(
+        "https://api.cloudinary.com/v1_1/prerit-cloud/image/upload",
+        {
+          method: "post",
+          body: data,
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          // console.log(data);
+          toast.success("Image Upload Sucessful ! Wait a Moment !", {
+            position: toast.POSITION.TOP_CENTER,
+          });
+          url = data.url;
+          console.log(url);
+        })
+        .catch((err) => {
+          // console.log(err);
+          toast.error("Image Upload Failed , Retry? ", {
+            position: toast.POSITION.TOP_CENTER,
+          });
+          return;
+        });
+    }
+    API();
+  }
+
+  function handleChange(e) {
+    setimage(e.target.files[0]);
+    setimgUrl(URL.createObjectURL(e.target.files[0]));
+  }
+
+  return (
+    <div>
+      <h2>Upload Image Here</h2>
+      <p class="lead">{/* No Plugins <b>Just Javascript</b> */}</p>
+
+      <form id="file-upload-form" class="uploader">
+        <input
+          id="file-upload"
+          type="file"
+          name="fileUpload"
+          onChange={handleChange}
+        />
+
+        <label for="file-upload" id="file-drag">
+          {imgUrl && (
+            <div
+              style={{
+                textAlign: "center",
+                margin: "auto",
+                width: "220px",
+                minHeight: "120px",
+                maxHeight: "auto",
+              }}
+            >
+              <img
+                id="file-image"
+                src={imgUrl}
+                alt="Preview"
+                style={{ width: "inherit" }}
+              />
+            </div>
+          )}
+          <div id="start">
+            <i class="fa fa-download" aria-hidden="true"></i>
+            <div>Select a file or drag here</div>
+            <div id="notimage" class="hidden">
+              Please select an image
+            </div>
+            <span id="file-upload-btn" class="btn btn-primary">
+              Select a file
+            </span>
+          </div>
+        </label>
+        <span id="file-upload-btn" class="btn btn-primary" onClick={PostData}>
+          Submit
+        </span>
+      </form>
+      <br />
+    </div>
+  );
+};
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Footer from "./Footer";
 import Main from "./Main";
+import { Activity } from "./Activity";
 function App() {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <div style={{ width: "100%", height: "74%" }}>
         <Main />
       </div>
+      <Activity />
       <div
         style={{ width: "100%", z: "10", position: "fixed" }}
         className="footer"
